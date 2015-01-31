e8fe532ec9d9c4ea660a60f6a02816c41e475e6e
nsarathsoman <nsarathsoman@gmail.com>
2015-01-31 16:32:09 +0530

    Initial Commit

---
+++ src/main/java/com/nibodha/nlight/NlightServiceApplication.java
@@ -0,0 +1,12 @@
+package com.nibodha.nlight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlightServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NlightServiceApplication.class, args);
+    }
+}
